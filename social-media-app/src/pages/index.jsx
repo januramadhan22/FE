30fe3ac02dd4bca293136770d51adbc4f1fc4810
Janu <abbyjunior600@gmail.com>
2022-10-18 22:24:01 +0700

    Finishing Navbar with modal and Add font family

---
+++ social-media-app/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ContainerLogin } from "../components/Container";
+
+function SosmedApp() {
+  return (
+    <ContainerLogin>
+      <div>Login</div>
+    </ContainerLogin>
+  );
+}
+
+export default SosmedApp;
