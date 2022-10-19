fb545d67dc29bb8d5c2115d7db123f17c80a09ef
Janu <abbyjunior600@gmail.com>
2022-10-19 11:41:52 +0700

    update formpost

--- social-media-app/src/index.js
+++ social-media-app/src/index.js
@@ -10,6 +10,6 @@ import SosmedApp from "./pages";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SosmedApp />
+    <Home />
   </React.StrictMode>
 );
