4935e48b6b3f61e541437bd348a607b5f21700e7
Janu <abbyjunior600@gmail.com>
2022-10-20 09:26:29 +0700

    Add Title for everypage and change logo app

--- social-media-app/src/pages/Home.jsx
+++ social-media-app/src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { ContainerHome } from "../components/Container";
 import { PostCard, FormPost } from "../components/Card";
+import { useTitle } from "../utils/hooks/useTitle";
 
 function Home() {
+  useTitle("Sosmed App");
+
   return (
     <ContainerHome>
       <div className="md:mx-96 mx-40 md:h-full">
         <FormPost />
         <PostCard />
       </div>
-
     </ContainerHome>
   );
 }
--- social-media-app/src/pages/index.jsx
+++ social-media-app/src/pages/index.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { ContainerLogin } from "../components/Container";
 import { PostCard, FormPost } from "../components/Card";
 import { WithRouter } from "../utils/Navigation";
+import { useTitle } from "../utils/hooks/useTitle";
 
 function SosmedApp() {
+  useTitle("Sosmed App");
   return (
     <ContainerLogin>
       <div className="md:mx-96 mx-40 cursor-not-allowed">
